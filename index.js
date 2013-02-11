e70c3da63369b0a4b69dc35c06a2924231ea1a89
Santiago Carot Nemesio <sancane@gmail.com>
2013-02-11 17:42:31 +0100

    Load virtual machine drivers

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var logger = require('nlogger').logger(module);
 var pkginfo = require('pkginfo')(module);
+var fs = require('fs');
 
-function load_drivers() {
+var drivers = [];
+
+function load_drivers(cb) {
+  var path = "./lib/drivers";
   logger.info("Loading drivers.");
+  fs.readdir(path, function(err, files) {
+    if (err) {
+      logger.error(err);
+      throw err;
+    }
+
+    for (var i = 0; i < files.length; i++) {
+      var Driver = require(path + '/' + files[i]);
+      drivers.push(new Driver);
+    }
+
+    cb();
+  });
 }
 
 
 logger.info('Daemon ', module.exports.name, ' version ',
   module.exports.version, ' starting');
 
-load_drivers();
+load_drivers(function() {
+  logger.info("Starting drivers drivers.");
+  for (var i = 0; i < drivers.length; i++)
+    drivers[i].start();
+});
