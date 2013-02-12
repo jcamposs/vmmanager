d12983f5be51f6ea3bd1954f2de7e2d82fb12d45
Santiago Carot Nemesio <sancane@gmail.com>
2013-02-12 10:04:30 +0100

    Daemon makes a synchronized initialization process

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 var logger = require('nlogger').logger(module);
 var pkginfo = require('pkginfo')(module);
+var nimble = require('nimble');
 var fs = require('fs');
 
 var drivers = [];
 
-function load_drivers(cb) {
-  var path = "./lib/drivers";
-  logger.info("Loading drivers.");
-  fs.readdir(path, function(err, files) {
-    if (err) {
-      logger.error(err);
-      throw err;
-    }
-
-    for (var i = 0; i < files.length; i++) {
-      var Driver = require(path + '/' + files[i]);
-      drivers.push(new Driver);
-    }
-
-    cb();
-  });
-}
-
-
 logger.info('Daemon ', module.exports.name, ' version ',
-  module.exports.version, ' starting');
+  module.exports.version, ' starting.');
 
 logger.debug("Installing signal handlers");
 process.on('SIGINT', function() {
@@ -37,8 +19,31 @@ process.on('SIGHUP', function() {
   logger.debug('TODO:');
 });
 
-load_drivers(function() {
-  logger.info("Starting drivers drivers.");
-  for (var i = 0; i < drivers.length; i++)
-    drivers[i].start();
+nimble.series([
+  function(callback) {
+    logger.info("Loading drivers.");
+    var path = "./lib/drivers";
+
+    fs.readdir(path, function(err, files) {
+      if (err) {
+        logger.error(err);
+        throw err;
+      }
+
+      for (var i = 0; i < files.length; i++) {
+        var Driver = require(path + '/' + files[i]);
+        drivers.push(new Driver);
+      }
+
+      callback();
+    });
+  },
+  function(callback) {
+    logger.info("Starting drivers drivers.");
+    for (var i = 0; i < drivers.length; i++)
+      drivers[i].start();
+    callback();
+  }
+], function() {
+  logger.info('Daemon ', module.exports.name, ' running.');
 });
