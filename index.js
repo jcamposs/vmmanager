8075e2afb7e77ec1276de1799935090e866a0688
Santiago Carot Nemesio <sancane@gmail.com>
2013-02-15 16:23:21 +0100

    Parse command options

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
 var daemon = require('./lib/daemon')
 
-daemon.start();
+var environment = "development";
+
+if (Object.config) {
+  console.error("Cant store daemon configuration.");
+  process.exit(1);
+} else {
+  Object.config = {
+    environment: "development"
+  }
+}
+
+parse_opts();
+daemon.start()
+
+function print_usage() {
+  console.log("Usage: node vmmanager [OPTIONS]");
+  console.log("Starts a vmmanager daemon to manage workspaces and virtual machines.");
+  console.log();
+  console.log("List of command line options:");
+  console.log("\t-e, --environment=ENVIRONMENT");
+  console.log("\t\tEnvironment for the process.");
+  console.log("\t\tPossible values: development, test, production");
+  console.log("\t\tDefault: development");
+  console.log("\t-h, --help");
+  console.log("\t\tShow this message.");
+  process.exit(0);
+}
+
+function set_environment(env) {
+  if (env != "development" && env != "test" && env != "production") {
+    console.error("Error: Invalid environment", env);
+    print_usage();
+  }
+
+  Object.config.environment = env;
+}
+
+function parse_opts() {
+  var i = 2;
+
+  while (i < process.argv.length) {
+    if (process.argv[i] == "-e") {
+      if (process.argv.length < i + 2)
+        print_usage();
+
+      set_environment(process.argv[i + 1]);
+      i += 2;
+    } else if (process.argv[i].indexOf("--environment=") == 0) {
+      set_environment(process.argv[i].substring("--environment=".length,
+                                                     process.argv[i].length));
+    } else if (process.argv[i] == "-h" ||
+                                       process.argv[i].indexOf("--help") == 0){
+      print_usage();
+    } else {
+      console.error("Error: Invalid option", process.argv[i]);
+      print_usage();
+    }
+  }
+}
